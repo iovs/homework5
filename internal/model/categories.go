f9f12c9c20d7d06247ad168e935ddb4daaaeb2b9
iovs <ovsyannikovio@gmail.com>
2025-02-14 14:01:41 +0300

    add categories struct

---
+++ internal/model/categories.go
@@ -0,0 +1,11 @@
+package categories
+
+//# Категория товара (структура)
+//Поля структуры категория:
+//- название категории (string)
+//- ID категории (int)
+
+type Category struct {
+	CategoryName string
+	CategoryID   int64
+}
